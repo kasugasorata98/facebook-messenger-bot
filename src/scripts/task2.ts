545dfb2739cb824952d52fcc3501a43f3626b6b2
kasugasorata98 <jeromykho98@gmail.com>
2023-03-04 20:07:31 +0800

    done

---
+++ src/scripts/task2.ts
@@ -0,0 +1,69 @@
+import Task2Orders from '../dump/task2order.json'
+interface Transaction {
+  id: number
+  sourceAccount: string
+  targetAccount: string
+  amount: number
+  category: string
+  time: string
+}
+function findDuplicateTransactions(transactions: Array<Transaction> = []) {
+  // split to groups based on similarities
+  const segregatedTransactions = transactions.reduce(
+    (accumulator, transaction) => {
+      const { amount, category, targetAccount, sourceAccount } = transaction
+      const key = `${amount}-${category}-${targetAccount}-${sourceAccount}`
+      if (!accumulator[key]) {
+        accumulator[key] = []
+      }
+      accumulator[key].push(transaction)
+      return accumulator
+    },
+    {} as Record<string, Transaction[]>
+  )
+
+  //sort by time
+  for (const groupKey of Object.keys(segregatedTransactions)) {
+    segregatedTransactions[groupKey].sort((a, b) => {
+      return new Date(a.time).getTime() - new Date(b.time).getTime()
+    })
+  }
+  const duplicatesWithin1Minute: Array<Array<Transaction>> = []
+  let count = 0
+  for (const group of Object.values(segregatedTransactions)) {
+    duplicatesWithin1Minute[count] = []
+    for (let i = 1; i < group.length; i++) {
+      const prevTransaction = group[i - 1]
+      const currentTransaction = group[i]
+      if (
+        (new Date(currentTransaction.time).getTime() -
+          new Date(prevTransaction.time).getTime()) /
+          1000 <
+        60
+      ) {
+        // means current and previous are duplicates under 1 minute
+        if (
+          duplicatesWithin1Minute[count].find(duplicate => {
+            return duplicate.id === prevTransaction.id
+          })
+        ) {
+          duplicatesWithin1Minute[count] = [
+            ...duplicatesWithin1Minute[count],
+            currentTransaction,
+          ]
+        } else {
+          duplicatesWithin1Minute[count] = [
+            ...duplicatesWithin1Minute[count],
+            prevTransaction,
+            currentTransaction,
+          ]
+        }
+      }
+    }
+    count++
+  }
+
+  return duplicatesWithin1Minute
+}
+
+console.log(findDuplicateTransactions(Task2Orders))
